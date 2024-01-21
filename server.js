7017c469ed1659fc9ab44b387bfc5a7405e88a53
benitaisaac <benita.isaac@gmail.com>
2024-01-20 16:56:08 -0800

    added get route for notes html

--- server.js
+++ server.js
@@ -6,24 +6,15 @@ const port = process.env.PORT || 3003;
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
-})
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+});
+
 
 app.listen(port, (req, res) => {
     console.log(`your app is running on ${port}`)
 }
-)
+);
 
-//Acceptance Criteria: 
-// GIVEN a note-taking application
-// WHEN I open the Note Taker
-// THEN I am presented with a landing page with a link to a notes page
-// WHEN I click on the link to the notes page
-// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
-// WHEN I enter a new note title and the note’s text
-// THEN a "Save Note" button and a "Clear Form" button appear in the navigation at the top of the page
-// WHEN I click on the Save button
-// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes and the buttons in the navigation disappear
-// WHEN I click on an existing note in the list in the left-hand column
-// THEN that note appears in the right-hand column and a "New Note" button appears in the navigation
-// WHEN I click on the "New Note" button in the navigation at the top of the page
-// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column and the button disappears
