c9057a8448e11d01d3a4702ed6608081f4e29bda
benitaisaac <benita.isaac@gmail.com>
2024-01-20 16:45:13 -0800

    completed get request for index.html

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express(); 
 const port = process.env.PORT || 3003; 
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
 app.listen(port, (req, res) => {
     console.log(`your app is running on ${port}`)
