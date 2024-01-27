cd630a1089148d31abe06f521002d74c9f837469
benitaisaac <benita.isaac@gmail.com>
2024-01-27 15:41:57 -0800

    created route for notes and added route to index.html

--- Routes/mainRoute.js
+++ Routes/mainRoute.js
@@ -1,4 +1,13 @@
 const router = require('express').Router();
+const notesRoute = require('./notesRoute');
+const path = require('path'); 
+
+//send to notesRoute
+router.use('/notes', notesRoute); 
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 
 module.exports = router;
