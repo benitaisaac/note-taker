8f59af9e37a5de312ea7a5952fa8926d8c196190
benitaisaac <benita.isaac@gmail.com>
2024-01-27 15:26:59 -0800

    added custom middleware and created routes folder for modularization

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const path = require('path');
+const mainRouter = require('./Routes/mainRoute');
+
+//import middleware
+const logger = require('./Middleware/logger');
 
 const app = express(); 
 const port = process.env.PORT || 3003; 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
-});
+//Middleware
+app.use('/', mainRouter);
+app.use(logger); 
+app.use(express.json); 
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'))
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/index.html'))
+// });
+
+// app.get('/notes', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/notes.html'))
+// });
+
+// app.post('/api/notes', (req, res) => {
+    
+// })
 
 
 app.listen(port, (req, res) => {
